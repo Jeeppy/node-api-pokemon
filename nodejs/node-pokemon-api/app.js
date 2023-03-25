3358b725f643e9df876caa33350b927f762dc8e3
Jean-Patrick Debaëne <jp.debaene@gmail.com>
2023-03-25 22:15:44 +0100

    feat (node-pokemon-api): add route with param

--- nodejs/node-pokemon-api/app.js
+++ nodejs/node-pokemon-api/app.js
@@ -5,4 +5,11 @@ const port = 3000;
 
 app.get('/', (req, res) => res.send("Hello again, express !"));
 
+app.get('/api/pokemon/:id', (req, res) => {
+  const id = req.params.id;
+  res.send(`Vous avez demandé le pokemon n° ${id}`)
+})
+
+app.get('/api/pokemon/')
+
 app.listen(port, () => console.log(`Notre application node est démarée sur : http://localhost:${port}`))
