8e031f544f66121113f4fb75ec6e74c74e8ab250
Jean-Patrick Debaëne <jp.debaene@gmail.com>
2023-03-25 21:35:57 +0100

    feat (node-pokemon-api): add nodemon

--- nodejs/node-pokemon-api/app.js
+++ nodejs/node-pokemon-api/app.js
@@ -3,6 +3,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send("Hello, express !"));
+app.get('/', (req, res) => res.send("Hello again, express !"));
 
 app.listen(port, () => console.log(`Notre application node est démarée sur : http://localhost:${port}`))
