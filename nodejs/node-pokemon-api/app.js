3418cc2d279641335b73f3548141359a7abc7430
Jean-Patrick Debaëne <jp.debaene@gmail.com>
2023-03-25 22:34:53 +0100

    feat (node-pokemon-api): add route to display pokemon's number and route to search pokemon with id

--- nodejs/node-pokemon-api/app.js
+++ nodejs/node-pokemon-api/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+let pokemons = require('./mock-pokemon');
 
 const app = express();
 const port = 3000;
@@ -6,10 +7,13 @@ const port = 3000;
 app.get('/', (req, res) => res.send("Hello again, express !"));
 
 app.get('/api/pokemon/:id', (req, res) => {
-  const id = req.params.id;
-  res.send(`Vous avez demandé le pokemon n° ${id}`)
+  const id = parseInt(req.params.id);
+  const pokemon = pokemons.find(pokemon => pokemon.id === id);
+  res.send(`Vous avez demandé le pokemon ${pokemon.name}.`)
 })
 
-app.get('/api/pokemon/')
+app.get('/api/pokemons/', (req, res) => {
+  res.send(`Il y a ${pokemons.length} pokémons dans le pokédex, pour le moment`);
+})
 
 app.listen(port, () => console.log(`Notre application node est démarée sur : http://localhost:${port}`))
