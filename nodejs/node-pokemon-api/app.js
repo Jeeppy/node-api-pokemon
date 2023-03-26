f2d6bcd41f2e26f412a442103db0568590bb7785
Jean-Patrick Debaëne <jp.debaene@gmail.com>
2023-03-26 15:10:31 +0200

    feat (node-pokemon-api): add json response

--- nodejs/node-pokemon-api/app.js
+++ nodejs/node-pokemon-api/app.js
@@ -1,19 +1,22 @@
-const express = require('express');
-let pokemons = require('./mock-pokemon');
+const express = require('express')
+const { success } = require ('./helper.js')
+let pokemons = require('./mock-pokemon')
 
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
 
-app.get('/', (req, res) => res.send("Hello again, express !"));
+app.get('/', (req, res) => res.send("Hello again, express !"))
 
 app.get('/api/pokemon/:id', (req, res) => {
-  const id = parseInt(req.params.id);
-  const pokemon = pokemons.find(pokemon => pokemon.id === id);
-  res.send(`Vous avez demandé le pokemon ${pokemon.name}.`)
+  const id = parseInt(req.params.id)
+  const pokemon = pokemons.find(pokemon => pokemon.id === id)
+  const message = "Un pokémon a bien été trouvé."
+  res.json(success(message, pokemon))
 })
 
 app.get('/api/pokemons/', (req, res) => {
-  res.send(`Il y a ${pokemons.length} pokémons dans le pokédex, pour le moment`);
+  const message = 'La liste des pokémons a bien été récupérée.'
+  res.json(success(message, pokemons))
 })
 
 app.listen(port, () => console.log(`Notre application node est démarée sur : http://localhost:${port}`))
