19cc38e42184322b795f57673c8a9441e7fba7b6
Jean-Patrick Debaëne <jp.debaene@gmail.com>
2023-03-26 18:35:26 +0200

    feat (node-pokemon-api): add route to create new pokemon and modify a pokemon

--- nodejs/node-pokemon-api/app.js
+++ nodejs/node-pokemon-api/app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const morgan = require('morgan')
 const favicon = require('serve-favicon')
-const { success } = require ('./helper.js')
+const bodyParser = require('body-parser')
+const { success, getUniqueId } = require ('./helper.js')
 let pokemons = require('./mock-pokemon')
 
 const app = express()
@@ -10,10 +11,11 @@ const port = 3000
 app
   .use(favicon(__dirname + '/favicon.ico'))
   .use(morgan('dev'))
+  .use(bodyParser.json())
 
 app.get('/', (req, res) => res.send("Hello again, express !"))
 
-app.get('/api/pokemon/:id', (req, res) => {
+app.get('/api/pokemons/:id', (req, res) => {
   const id = parseInt(req.params.id)
   const pokemon = pokemons.find(pokemon => pokemon.id === id)
   const message = "Un pokémon a bien été trouvé."
@@ -23,6 +25,24 @@ app.get('/api/pokemon/:id', (req, res) => {
 app.get('/api/pokemons/', (req, res) => {
   const message = 'La liste des pokémons a bien été récupérée.'
   res.json(success(message, pokemons))
+})
+
+app.post('/api/pokemons', (req, res) => {
+  const id = getUniqueId(pokemons)
+  const  pokemonCreated = { ...req.body, ...{id:id, created: new Date()}}
+  pokemons.push(pokemonCreated)
+  const message = `Le pokémon ${pokemonCreated.name} a bien été créé.`
+  res.json(success(message, pokemonCreated))
+})
+
+app.put('/api/pokemons/:id', (req, res) => {
+  const id = parseInt(req.params.id)
+  const pokemonUpdated = { ...req.body, id:id }
+  pokemons = pokemons.map(pokemon => {
+    return pokemon.id === id ? pokemonUpdated : pokemon
+  })
+  const message = `Le pokémon ${pokemonUpdated.name} a bien été modifié.`
+  res.json(success(message, pokemonUpdated))
 })
 
 app.listen(port, () => console.log(`Notre application node est démarée sur : http://localhost:${port}`))
